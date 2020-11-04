021556f5e92c2130d224d0cd87c37456501e0746
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-04 13:23:21 -0800

    edited html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-var textArea = Array.from(document.querySelectorAll('textarea'))
 function currentTime() {
     var day = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(day)
 }
 setInterval(currentTime, 1000);
 
-$(textArea).each(function () {
+
+$('textarea').each(function () {
     var hr = moment().format('H')
     var dataIndex = $(this).data('index')
+    console.log(dataIndex)
     if (hr == dataIndex) {
         $(this).addClass('present')
     }
@@ -18,3 +19,4 @@ $(textArea).each(function () {
         $(this).addClass('past')
     }
 })
+
