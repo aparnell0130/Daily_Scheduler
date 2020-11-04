4746e1dbe8c3fb310fc9a7958e5365478f042bfc
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-03 16:37:17 -0800

    edited html, added functions for current time and to color text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-var currentTime = function () {
-    $("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+var textArea = Array.from(document.querySelectorAll('textarea'))
+function currentTime() {
+    var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(day)
 }
 setInterval(currentTime, 1000);
+
+$(textArea).each(function () {
+    var hr = moment().format('H')
+    var dataIndex = $(this).data('index')
+    if (hr == dataIndex) {
+        $(this).addClass('present')
+    }
+    else if (hr <= dataIndex) {
+        $(this).addClass('future')
+
+    }
+    else {
+        $(this).addClass('past')
+
+    }
+})
