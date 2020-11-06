a18c9dd80169358d5cc5a3127804fa016fbfeffb
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-06 12:39:46 -0800

    added gif for readme, edited js, added comments for js

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Daily_Scheduler
+
+
+# Code_Quizzer
+## Description
+
+
+
+### Question Example Code
+```JS
+
+```
+
+
+
+### Tools/Technologies
+- JavaScript
+- HTML
+- CSS
+
+### Generate Random Question Code
+```JS
+
+
+```
+## Live game link
+- [Code Quizzer](https://aparnell0130.github.io/Code_Quizzer/)
+
+![ALT Text](assets/images/codeQuizzer.gif)
+
+## License
+- N/A 
+
+## Author Info
+- Linkedin - [Aaron Parnell](https://www.linkedin.com/in/aaron-parnell-1ab4661b3/)
+- Github - [aparnell0130](https://github.com/aparnell0130)
+
+[Back to top](#Code_Quizzer)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,44 @@
+// function to display current time on screen
 function currentTime() {
     var day = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(day)
 }
+// keep time running
 setInterval(currentTime, 1000);
 
-$('.hr-9').val(localStorage.getItem('9'))
-$('.hr-10').val(localStorage.getItem('10'))
-$('.hr-11').val(localStorage.getItem('11'))
-$('.hr-12').val(localStorage.getItem('12'))
-$('.hr-13').val(localStorage.getItem('13'))
-$('.hr-14').val(localStorage.getItem('14'))
-$('.hr-15').val(localStorage.getItem('15'))
-$('.hr-16').val(localStorage.getItem('16'))
-$('.hr-17').val(localStorage.getItem('17'))
-
+// create object from local storage keys and values
+var storageObject = {
+    storageKey: Object.keys(localStorage),
+    storageValue: Object.values(localStorage)
+}
+// check each textarea and fill in color based on time
 $('textarea').each(function () {
     var hr = moment().format('H')
     var dataIndex = $(this).data('index')
-
-    if (hr == dataIndex) {
+    var areaHr = this.id
+    console.log(areaHr)
+    if (hr == areaHr) {
         $(this).addClass('present')
     }
-    else if (hr <= dataIndex) {
+    else if (hr <= areaHr) {
         $(this).addClass('future')
     }
     else {
         $(this).addClass('past')
     }
+    // loop through keys and values
+    for (let i = 0; i < storageObject.storageKey.length && storageObject.storageValue.length; i++) {
+        const keyIndex = storageObject.storageKey[i];
+        const keyValue = storageObject.storageValue[i]
+        // if storage key is = to textarea data index
+        if (dataIndex == keyIndex) {
+            // make target value the key value
+            $(this).val(keyValue)
+        }
+    }
 })
 
+// add click event for each button set sibling index and value in local storage
 $('button').click(function (event) {
     event.preventDefault()
     var textAttr = $(this).prev().data('index')
