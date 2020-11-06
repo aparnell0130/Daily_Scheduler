9841688701bf9662848bd804345eaaa7c4aeff5a
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-05 23:31:47 -0800

    added function for each click, added data from local storage to text, added class in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,20 @@ function currentTime() {
 }
 setInterval(currentTime, 1000);
 
+$('.hr-9').val(localStorage.getItem('9'))
+$('.hr-10').val(localStorage.getItem('10'))
+$('.hr-11').val(localStorage.getItem('11'))
+$('.hr-12').val(localStorage.getItem('12'))
+$('.hr-13').val(localStorage.getItem('13'))
+$('.hr-14').val(localStorage.getItem('14'))
+$('.hr-15').val(localStorage.getItem('15'))
+$('.hr-16').val(localStorage.getItem('16'))
+$('.hr-17').val(localStorage.getItem('17'))
 
 $('textarea').each(function () {
     var hr = moment().format('H')
     var dataIndex = $(this).data('index')
-    console.log(dataIndex)
+
     if (hr == dataIndex) {
         $(this).addClass('present')
     }
@@ -20,3 +29,11 @@ $('textarea').each(function () {
     }
 })
 
+$('button').click(function (event) {
+    event.preventDefault()
+    var textAttr = $(this).prev().data('index')
+    var text = $(this).prev().val()
+    if ($(this).data('number')) {
+        localStorage.setItem(textAttr, text)
+    }
+})
